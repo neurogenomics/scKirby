85f147cc37d030d88a08a099d4da95de95947b76
Brian M. Schilder <34280215+bschilder@users.noreply.github.com>
2023-04-28 00:28:43 +0100

    Fix Seurat upgrader [skip ci]

--- R/seurat1_to_list.R
+++ R/seurat1_to_list.R
@@ -5,9 +5,9 @@ seurat1_to_list <- function(obj,
                             verbose=TRUE){
 
   messager("+ Seurat (V1) ==> list",v=verbose)
-  list(data=list(counts=obj@raw.data,
-                 data=obj@data,
-                 scale.data=obj@scale.data),
+  list(data=list(RNA.counts=obj@raw.data,
+                 RNA.data=obj@data,
+                 RNA.scale.data=obj@scale.data),
        obs=obj@data.info,
        var=obj@mean.var,
        reductions=list(PCA=list(embeddings=obj@pca.x,
